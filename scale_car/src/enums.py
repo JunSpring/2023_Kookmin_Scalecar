50a71d283288757f5c8037a1daa00b415ad36f58
JunSpring <wjdwnsqja08@gmail.com>
2023-08-07 23:44:03 +0900

    edit enum(StateNum)

--- scale_car/src/enums.py
+++ scale_car/src/enums.py
@@ -1,6 +1,7 @@
 from enum import IntEnum
 
-class StateNum(IntEnum): 
+class StateNum(IntEnum):
+    NORMAL_DRIVING                      = 0
     SCHOOL_ZONE_SIGN_RECOGNITION        = 1
     SCHOOL_ZONE_CROSSING_RECOGNITION    = 2
     SCHOOL_ZONE_RESTART                 = 3
