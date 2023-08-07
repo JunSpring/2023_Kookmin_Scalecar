011a1aafd02c41fd7b751c19649dc3298dada04d
JunSpring <wjdwnsqja08@gmail.com>
2023-08-07 21:44:29 +0900

    edit enum

--- scale_car/src/state_number.py
+++ scale_car/src/state_number.py
@@ -5,6 +5,5 @@ class StateNum(IntEnum):
     SCHOOL_ZONE_CROSSING_RECOGNITION    = 2
     SCHOOL_ZONE_RESTART                 = 3
     DYNAMIC_OBSTACLE                    = 4
-    RUBBERCON_PRE_STATE                 = 5
-    RUBBERCON_DRIVING                   = 6
-    STATIC_OBSTACLE                     = 7
+    RUBBERCON_DRIVING                   = 5
+    STATIC_OBSTACLE                     = 6
