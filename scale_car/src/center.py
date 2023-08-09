b77029de733955565ef0b270024a293ac4c4af07
JunSpring <wjdwnsqja08@gmail.com>
2023-08-09 14:59:02 +0900

    edit center

--- scale_car/src/center.py
+++ scale_car/src/center.py
@@ -75,8 +75,8 @@ class LidarReceiver():
 
         # ros가 실행되는 동안 publish_data 함수 반복실행
         while not rospy.is_shutdown():
-            self.select_mission_number()
-            self.publish_data()
+            self.excute_mission()
+            # self.publish_data()
 
     # 객체 검출 subscribe callback 함수
     def Obstacles_callback(self, data):
@@ -124,7 +124,8 @@ class LidarReceiver():
     def excute_mission(self):
         global state
 
-        if state == StateNum.NORMAL_DRIVING or state is None:
+        if state == StateNum.NORMAL_DRIVING or state is None: 
+            self.normal_driving()
             self.select_mission_number()
 
         else:
@@ -185,237 +186,45 @@ class LidarReceiver():
                 min_distance = distance_to_center
 
         return min_distance
+    
+    def normal_driving(self):
+        rospy.loginfo("normal_driving")
 
     # mission1 어린이보호구역 함수
     def school_zone(self):
-        # global 변수
         global state
-        global sign
-        global mission1_start
-        global mission1_running
-        global mission1_prev_time
 
-        # mission1 start 직후
-        if not mission1_start:
-            mission1_start = rospy.Time.now().to_sec()
-            state = 1
-        
-        # mission start 직후의 이후
-        else:
-            time = rospy.Time.now().to_sec() - mission1_start
-            if time < 15:
-                state = 1
-                if time > mission1_prev_time:
-                    rospy.loginfo('어린이 보호구역 %d',int(time))
-                    mission1_prev_time += 1
-            else:
-                # 변수 초기화
-                self.end_mission()
-                mission1_start = None
-                mission1_running = False
-                mission1_prev_time = 0
-                sign = None
+        rospy.loginfo("school_zone")
+
+        if state == StateNum.SCHOOL_ZONE_SIGN_RECOGNITION:
+            pass
+
+        elif state == StateNum.SCHOOL_ZONE_CROSSING_RECOGNITION:
+            pass
+
+        elif state == StateNum.SCHOOL_ZONE_RESTART:
+            pass
 
     # mission2 동적장애물 함수
-    def dynamic_object(self, mission_start, circles = None):
+    def dynamic_object(self):
         # global 변수
         global state
-        global mission2_count
-        global mission2_prev_data
-        global mission2_start
-        global mission2_prev_time
 
-        closest_circle = None
-        min_distance = 100
-        
-        # 미션 시작 전 mission2인지 판단하기 위한 코드
-        if not mission_start:
-            if circles:
-                # 가장 가까운 circle 판단 코드
-                for circle in circles:
-                    distance = math.sqrt(circle.center.x ** 2 + circle.center.y ** 2) - circle.true_radius
-                    if distance < min_distance:
-                        closest_circle = circle
-                        min_distance = distance
-
-                # 객체의 x좌표가 감소할 때마다 count하는 코드
-                if not mission2_prev_data:
-                    mission2_prev_data = closest_circle.center.y
-                else:
-                    # rospy.loginfo(mission2_prev_data - closest_circle.center.y)
-                    if mission2_prev_data - closest_circle.center.y > 0.015:
-                        
-                        mission2_count += 1
-                    mission2_prev_data = closest_circle.center.y
-                # rate = rospy.Rate(10)
-
-                # for circle in circles:
-                #     distance = math.sqrt(circle.center.x ** 2 + circle.center.y ** 2) - circle.true_radius
-                #     if distance < min_distance:
-                #         closest_circle = circle
-                #         min_distance = distance
-
-                # if not mission2_prev_data:
-                #     mission2_prev_data = closest_circle.center.y
-
-                # for i in range(15):
-                #     rospy.loginfo(i)
-                #     rate.sleep()
-
-                # if mission2_prev_data - closest_circle.center.y > 0.3:
-                #     mission2_count = 20
-
-        # mission2가 맞다면 실행하는 코드
-        else:
-            # mission2 start 직후
-            if not mission2_start:
-                mission2_start = rospy.Time.now().to_sec()
-                state = 2
-
-            # mission2 start 직후의 이후
-            else:
-                time = rospy.Time.now().to_sec() - mission2_start
-                if time < 9:
-                    if time > mission2_prev_data:
-                        rospy.loginfo("동적장애물 %d", int(time))
-                        mission2_prev_time += 1
-                else:
-                    # 변수 초기화
-                    self.end_mission()
-                    mission2_count = 0
-                    mission2_prev_data = None
-                    mission2_start = None
-                    mission2_prev_time = 0
+        rospy.loginfo("dynamic_object")
 
     # mission3 라바콘
     def rubber_cone(self, mission_state, circles = None):
         # global 변수
         global state
 
-        # 가장 가까운 두 circle
-        circle1 = None
-        circle2 = None
-        circle1_error = True
-        circle2_error = True
-        error = False
-        min_distance = 100
-
-        if mission_state:
-            state = 3
-
-            if circles:
-                # 가장 가까운 두 circle을 찾는 코드
-                for circle in circles:
-                    distance = math.sqrt(circle.center.x ** 2 + circle.center.y ** 2) - circle.true_radius
-                    if distance < min_distance and circle.center.y > -0.1: # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                        circle1 = circle
-                        min_distance = distance
-                        circle1_error = False
-
-                if not circle1_error:
-                    circles.remove(circle1)
-
-                min_distance = 100
-                for circle in circles:
-                    distance = math.sqrt(circle.center.x ** 2 + circle.center.y ** 2) - circle.true_radius
-                    try:
-                        if distance < min_distance and circle.center.y < circle1.center.y - 0.45:
-                            circle2 = circle
-                            min_distance = distance
-                            circle2_error = False
-                    except:
-                        if distance < min_distance and circle.center.y < 0.3:
-                            circle2 = circle
-                            min_distance = distance
-                            circle2_error = False
- 
-                # try:
-                #     str = f"{circle1.center.y}\t{-1*circle1.center.x}\n{circle2.center.y}\t{-1*circle2.center.x}\n"
-                #     rospy.loginfo(str)
-                # except:
-                #     pass
-
-                if not circle1_error and not circle2_error and math.sqrt((circle1.center.x - circle2.center.x) ** 2 + (circle1.center.y - circle2.center.y) ** 2) > 0.7:
-                    circle_error = False
-                else:
-                    circle_error = True
-
-                if circle_error:
-                    if circle1_error:
-                        self.xwaypoint = -1
-                        self.ywaypoint = 1
-                    elif circle2_error:
-                        self.xwaypoint = -1
-                        self.ywaypoint = -1
-                    elif not error:
-                        self.ywaypoint = -1 * self.ywaypoint
-                        error = True
-                    else:
-                        if not circle_error:
-                            error = False
-
-                # 두 circle의 좌표의 평균값을 계산하여 저장
-                else:
-                    self.xwaypoint = (circle1.center.x + circle2.center.x) / 2
-                    self.ywaypoint = (circle1.center.y + circle2.center.y) / 2
-
-        else:
-            # 변수 초기화
-            self.end_mission()
-            self.xwaypoint = 0
-            self.ywaypoint = 0  
+        rospy.loginfo("rubber_cone")
 
     # mission4 정적장애물 함수
     def static_object(self, circles):
         # global 변수
         global state
-        global mission4_count
-        global mission4_state
-        global mission4_start
-        global mission4_prev_time
 
-        detected_circle = None
-
-        rate = rospy.Rate(10)
-
-        if circles:
-            # 가장 가까운 circle 판단 코드
-            for circle in circles:
-                if -0.1 <= circle.center.y <= 0.1:
-                    detected_circle = circle
-
-        if not mission4_state:
-            try:
-                if detected_circle:
-                    state = 4
-                    mission4_state = True
-                    mission4_start = rospy.Time.now().to_sec()
-                else:
-                    self.end_mission()
-                    mission4_count = 0
-                    mission4_state = False
-                    mission4_start = None
-            except:
-                self.end_mission()
-                mission4_count = 0
-                mission4_state = False
-                mission4_start = None
-
-        if mission4_state:
-            if mission4_count <= 5:
-                rate.sleep()
-                mission4_count += 1
-                return
-            else:
-                state = 0
-
-        if mission4_start and rospy.Time.now().to_sec() - mission4_start > 1.75:
-            # 변수 초기화
-            self.end_mission()
-            mission4_count = 0
-            mission4_state = False
-            mission4_start = None
-            mission4_prev_time = 0
+        rospy.loginfo("static_object")
 
     # 공통되는 변수를 초기화하는 함수
     def end_mission(self):
